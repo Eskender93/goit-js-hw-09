7b8253be2f4d81eb032095932fd62d54fff830d8
Eskender93 <120672710+Eskender93@users.noreply.github.com>
2023-06-04 15:21:46 +0000

    Deploying to gh-pages from @ Eskender93/goit-js-hw-09@fbb94be42710a407d2c5b7e171db9e4e4a483487 🚀

---
+++ 01-color-switcher.5c6eb9c3.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=0;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.5c6eb9c3.js.map
